708478052f7b3ac4855843f1f0142080e7512cf2
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:17:18 +0800

    Delete strelka.sh

--- strelka.sh
+++
@@ -1,3 +0,0 @@
-{strelka} --normalBam={Nbam} --tumorBam={Tbam} --referenceFasta={genome} --runDir={dir}
-./runWorkflow.py -m local -j {num}
-{pipeDir}/FilterStrelka.pl -s {dir}/results/variants/somatic.snvs.vcf.gz -i {dir}/results/variants/somatic.indels.vcf.gz > {dir}/somatic.strelka.vcf 2> {dir}/som
