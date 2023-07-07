8598358d46333d797bc165084ac630f2dc65298a
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:22:17 +0800

    Delete lumpy.sh

--- lumpy.sh
+++
@@ -1,7 +0,0 @@
-sample=$1
-Tbam=$2
-Nbam=$3
-
-smoove call -x --name $sample --exclude btu356_LCR-hs37d5.bed --fasta hg19.fa -p 8 --genotype $Tbam $Nbam
-
-perl lumpySomatic.pl -i $sample-smoove.genotyped.vcf.gz > $sample.vcf
