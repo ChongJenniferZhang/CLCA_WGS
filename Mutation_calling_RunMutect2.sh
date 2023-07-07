e10d6bed332588bfbd0cfe5407f5eae2212e7e0b
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:20:21 +0800

    Delete Mutation_calling_RunMutect2.sh

--- Mutation_calling_RunMutect2.sh
+++
@@ -1,2 +0,0 @@
-gatk --java-options "-Xmx8g" Mutect2 -R hg19.fa -I Tbam -tumor Tsample -I Nbam -normal Nsample -O gatk4.vcf.gz -pon Wgs.PoN.vcf.gz -L chr --native-pair-hmm-threads 8
-gatk --java-options "-Xmx2g" FilterMutectCalls -V gatk4.vcf.gz -O raw.vcf.gz -XL wgEncodeDacMapabilityConsensusExcludable.bed
