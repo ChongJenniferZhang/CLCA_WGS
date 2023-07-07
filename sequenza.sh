bf8ab9c90b2a85d6d7b44f2b00f3e864959989d6
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:18:02 +0800

    Delete sequenza.sh

--- sequenza.sh
+++
@@ -1,2 +0,0 @@
-{sequenza-utils} bam2seqz -n normal.bam -t tumor.bam -gc {pipeDir}/hg19.gc50Base.txt.gz -F /share/public/database/hg19/hg19.fa -S /share/public/software/samtools/0.1.18/samtools | {sequenza-utils} seqz_binning -w 50 -s - | gzip > sample.samll_seqs.gz
-Rscript {pipeDir}/sequenza.R sample.samll_seqs.gz {sample}
