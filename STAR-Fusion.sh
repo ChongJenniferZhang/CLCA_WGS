65b2f1bd41887da0a47c65daceeddae84f9ca444
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:28:04 +0800

    Delete STAR-Fusion.sh

--- STAR-Fusion.sh
+++
@@ -1,4 +0,0 @@
-j=Chimeric.out.junction
-prx=$1
-n=Log.final.out
-/share/public/software/Onc_Soft/STAR-Fusion-STAR-Fusion-v1.8.1/STAR-Fusion --chimeric_junction $j --genome_lib_dir fusion/lib/ctat_genome_lib_build_dir/ --output_dir $prx --CPU 8 --totalnumberreads $(cat $n | grep 'Number of input reads' | perl -lane 'print $F[-1]') --examine_coding_effect
