9941eb92b92d2dfb48bc0eddb11681a82c3e4920
ChongJenniferZhang <zhang_c@pku.edu.cn>
2023-07-07 15:27:33 +0800

    Delete adjust.R

--- adjust.R
+++
@@ -1,8 +0,0 @@
-args=commandArgs(TRUE)
-d<-read.table(args[1], header=T)
-d=d[order(d$Brown_observed),]
-p=d$Brown_observed
-adj = p.adjust(p, method='BH')
-d$q = adj
-
-write.table(d,file=args[2], quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
